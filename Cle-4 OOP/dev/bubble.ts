11600a3e1e4144e9687d958b26c37f789e403d41
Raymond <raymondhvh@hotmail.com>
2016-05-23 10:54:27 +0200

    folder structuur basis aangelegd
    Met deze folder structuur een basis gemaakt voor aparte mappen voor
    objecten en mini games.
    
    http://www.codebelt.com/typescript/object-oriented-programming-with-typescript/
    Voor meer informatie met werken met typescript en folders
    
    ///<reference path='Vehicle.ts'/>
    ///<reference path='../accessories/Tire.ts'/>
    ///<reference path='../../constants/TireType.ts'/>
    ///<reference path='../accessories/soundsystems/CDPlayer.ts'/>

--- Cle-4 OOP/dev/bubble.ts
+++
@@ -1,9 +0,0 @@
-class Bubble{
-    constructor(){
-        var posx =  Math.round(Math.random() * window.innerWidth)-120;
-		var newbubble = document.createElement("bubble");
-		document.body.appendChild(newbubble);
-		newbubble.style.left = posx+ "px";
-    }
-    
-}
